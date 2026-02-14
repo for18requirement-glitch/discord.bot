7a11a76adb0b5e8083e07db341493f3b864c8ebb
for18requirement-glitch <for18requirement@gmail.com>
2026-02-14 19:29:32 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot is online as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
